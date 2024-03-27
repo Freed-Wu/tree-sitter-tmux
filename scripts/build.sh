9d498162733affd40f4078b3f8707126773c5f42
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-03-27 14:25:45 +0800

    :fire: Remove code

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$(readlink -f "$0")")/.."
-
-tree-sitter generate
-cmake -Bbuild -DCMAKE_INSTALL_FULL_LIBDIR="${1:-$HOME/.local/share/nvim/repos/github.com/nvim-treesitter/nvim-treesitter}"
-cmake --build build
-cmake --install build
