6bb299ac6a3333f83d1a46cabc576fbe1376f4b9
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-07-12 07:09:34 +0800

    :boom: Inject format string by tmuxf
    Remove support of mixed string
    Inject shell by bash
    
    Currently, in order to handle tmux format string, many complex code are written, which increase large build time (9-12s) and difficulty for maintenance. By injecting tree-sitter-tmuxf, these code can be deleted.

--- queries/injections.scm
+++ queries/injections.scm
@@ -3,3 +3,29 @@
 
 ((comment) @injection.content
   (#set! injection.language "comment"))
+
+(set_option_directive
+  (command)
+  (option) @_option
+  (value
+    content: (string) @injection.content)
+  (#any-of? @_option
+    "set-titles-string" "window-status-format" "window-status-current-format" "status-left"
+    "status-right")
+  (#set! injection.language "tmuxf"))
+
+(inputs
+  content: (string) @injection.content
+  (#set! injection.language "tmuxf"))
+
+(if_statement
+  (if_keyword)
+  (condition
+    (string) @injection.content)
+  (#set! injection.language "tmuxf"))
+
+(if_statement
+  (elif_keyword)
+  (condition
+    (string) @injection.content)
+  (#set! injection.language "tmuxf"))
