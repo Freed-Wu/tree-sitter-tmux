a03e0b76998d6a08ea33d14bc11409c8996f4ea7
Freed-Wu <32936898+Freed-Wu@users.noreply.github.com>
2026-07-19 14:41:52 +0000

    chore: update generated artifacts

--- src/parser.c
+++ src/parser.c
@@ -71090,7 +71090,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_tmux(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
