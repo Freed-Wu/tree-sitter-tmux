bd334851188206824595987350c0bfb60ff76f75
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-07-24 16:36:08 +0800

    :ambulance: Fix comment body

--- grammar.js
+++ grammar.js
@@ -916,7 +916,7 @@ export default grammar({
         $._command_string,
       ),
 
-    comment: $ => seq('#', /\s*/, alias(/[^\n]*/, $.body)),
+    comment: $ => seq('#', /[ \t\v]*/, alias(/[^\n]*/, $.body)),
     _space: _ => prec(-1, repeat1(/[ \t]/)),
   },
 });
